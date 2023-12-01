505043dbbc7dc4532061352392dae956f5a4d310
Rlazyrk <rlazyrk@gmail.com>
2023-12-01 11:52:56 +0200

    one more fix and file name fix

--- lab6/lvl3/src/knuth_morris_pratt_algorithm.py
+++ lab6/lvl3/src/knuth_morris_pratt_algorithm.py
@@ -1,5 +1,12 @@
-def knuth_morris_prat(haystack, needle):
-    prefix = build_prefix(needle)
+def knuth_morris_prat(haystack: str, needle: str) -> list[int]:
+    """
+    Search for occurrences of the given pattern in the input text using the Knuth-Morris-Pratt algorithm.
+
+    :param haystack: The text in which we search for occurrences.
+    :param needle: The pattern we are searching for.
+    :return: A list containing the starting indices of all occurrences of the pattern in the text.
+    """
+    prefix = build_prefix_table(needle)
     needle_index = 0
     haystack_index = 0
     result = []
@@ -10,14 +17,19 @@ def knuth_morris_prat(haystack, needle):
             if needle_index == len(needle):
                 result.append(haystack_index - needle_index)
                 needle_index = prefix[needle_index - 1]
-        elif check_miss_math_needle_not_zero(needle, needle_index, haystack, haystack_index):
+        elif mismatch_and_not_zero(needle, needle_index, haystack, haystack_index):
             needle_index = prefix[needle_index - 1]
         else:
             haystack_index += 1
     return result
 
 
-def build_prefix(pattern):
+def build_prefix_table(pattern: str) -> list[int]:
+    """
+    Funk to build prefix table for patter
+    :param pattern: The pattern for which the prefix table is built.
+    :return: A list representing the prefix table.
+    """
     prefix = [0] * len(pattern)
     start = 0
     end = 1
@@ -34,5 +46,7 @@ def build_prefix(pattern):
     return prefix
 
 
-def check_miss_math_needle_not_zero(needle, needle_index, haystack, haystack_index):
+def mismatch_and_not_zero(
+    needle: str, needle_index: int, haystack: str, haystack_index: int
+) -> bool:
     return needle[needle_index] != haystack[haystack_index] and needle_index != 0
--- lab6/lvl3/test/knuth_morris_pratt_test.py
+++ lab6/lvl3/test/knuth_morris_pratt_test.py
@@ -1,12 +1,13 @@
 import unittest
 
-from lab6.lvl3.src.KMP_alg import knuth_morris_prat
+from lab6.lvl3.src.knuth_morris_pratt_algorithm import knuth_morris_prat
 
 
 class KmpTest(unittest.TestCase):
     def test_full_match(self):
         self.assertEqual(
-            knuth_morris_prat("aaaaaaaaaaaaaaa", "aaa"), [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+            knuth_morris_prat("aaaaaaaaaaaaaaa", "aaa"),
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
         )
 
     def test_no_match(self):
